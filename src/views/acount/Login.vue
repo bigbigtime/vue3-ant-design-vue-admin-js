7c59afcb25abd5a4264b207d2939a6749a1f2563
bigbigtime <409019683@qq.com>
2020-11-26 20:24:08 +0800

    account

--- src/views/acount/Login.vue
+++ src/views/acount/Login.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="account">
+    <div id="bbbb">adsfasdfsd</div>
     <div class="form-wrap">
       <a-form
         name="custom-validation"
@@ -16,8 +17,8 @@
         </a-form-item>
 
         <a-form-item>
-          <div id="captcha-dom" class="nc-container"></div>
-          <!--No-Captcha渲染的位置，其中 class 中必须包含 nc-container-->
+          <Captcha />
+          <!-- <-Captcha></Captcha> -->
         </a-form-item>
 
         <a-form-item>
@@ -35,62 +36,25 @@
 </template>
 
 <script>
-import { onMounted, reactive } from "vue";
-import "./captcha.js";
+import { onMounted, reactive, toRefs, ref } from "vue";
+// 局部组件（导入）
+import Captcha from "@/components/Captcha";
 export default {
   name: "Login",
+  components: { Captcha },
   setup(props){
-
-    const formConfig = reactive({
+    const formConfig = reactive({  // 类似于JSON对象的语法
       layout: {
-        labelCol: { span: 4 },
+        labelCol: { span: 10 },
         wrapperCol: { span: 14 },
       }
     })
+    const data = toRefs(formConfig);
 
-    onMounted(() => {
-        var nc_token = ["FFFF0N00000000009931", (new Date()).getTime(), Math.random()].join(':');  // 没有做任何的响应
-        var NC_Opt = 
-        {
-            renderTo: "#captcha-dom",
-            appkey: "FFFF0N00000000009931",
-            scene: "nc_login",
-            token: nc_token,
-            customWidth: 300,
-            trans:{"key1":"code0"},
-            elementID: ["usernameID"],
-            is_Opt: 0,
-            language: "cn",
-            isEnabled: true,
-            timeout: 3000,
-            times:5,
-            apimap: {
-                // 'analyze': '//a.com/nocaptcha/analyze.jsonp',
-                // 'get_captcha': '//b.com/get_captcha/ver3',
-                // 'get_captcha': '//pin3.aliyun.com/get_captcha/ver3'
-                // 'get_img': '//c.com/get_img',
-                // 'checkcode': '//d.com/captcha/checkcode.jsonp',
-                // 'umid_Url': '//e.com/security/umscript/3.2.1/um.js',
-                // 'uab_Url': '//aeu.alicdn.com/js/uac/909.js',
-                // 'umid_serUrl': 'https://g.com/service/um.json'
-            },   
-            callback: function (data) { 
-                window.console && console.log(nc_token)
-                window.console && console.log(data.csessionid)
-                window.console && console.log(data.sig)
-            }
-        }
-        var nc = new noCaptcha(NC_Opt)
-        nc.upLang('cn', {
-            _startTEXT: "请按住滑块，拖动到最右边",
-            _yesTEXT: "验证通过",
-            _error300: "哎呀，出错了，点击<a href=\"javascript:__nc.reset()\">刷新</a>再来一次",
-            _errorNetwork: "网络不给力，请<a href=\"javascript:__nc.reset()\">点击刷新</a>",
-        })
-    })
+    onMounted(() => {})
 
     return {
-      formConfig
+      ...data
     }
   }
 };
